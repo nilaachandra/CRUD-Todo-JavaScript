e36ab71a928157c0f1e1c6e3ddae389a7ae65338
Nila Chandra Laishram <nilalaishram@gmail.com>
2023-12-03 23:09:26 +0530

    final update

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.addEventListener('load', () => {
             redAlert.style.margin = "10px"
         } else {
             redAlert.innerHTML = "To Do added. Add more todos!";
-            redAlert.style.color = "Green"
+            redAlert.style.color = "Red"
             redAlert.style.margin = "10px"
             // Create a new todo object
             const todo = {
@@ -61,103 +61,104 @@ window.addEventListener('load', () => {
 
 // Function to display todos in the DOM
 function DisplayTodos() {
-	// Select the todo list container
-	const todoList = document.querySelector('#todo-list');
-	// Clear the existing content of the todo list container
-	todoList.innerHTML = "";
+    // Select the todo list container
+    const todoList = document.querySelector('#todo-list');
+    // Clear the existing content of the todo list container
+    todoList.innerHTML = "";
 
-	// Loop through each todo in the todos array
-	todos.forEach(todo => {
-		// Create a new div for the todo item
-		const todoItem = document.createElement('div');
-		todoItem.classList.add('todo-item');
+    // Loop through each todo in the todos array
+    todos.forEach(todo => {
+        // Create a new div for the todo item
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item');
 
-		// Create DOM elements for the todo item components
-		const label = document.createElement('label');
-		const input = document.createElement('input');
-		const span = document.createElement('span');
-		const content = document.createElement('div');
-		const actions = document.createElement('div');
-		const edit = document.createElement('button');
-		const deleteButton = document.createElement('button');
+        // Create DOM elements for the todo item components
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteButton = document.createElement('button');
 
-		// Set attributes and classes for the checkbox, span, and other elements
-		input.type = 'checkbox';
-		input.checked = todo.done;
-		span.classList.add('bubble');
-		content.classList.add('todo-content');
-		actions.classList.add('actions');
-		edit.classList.add('edit');
-		deleteButton.classList.add('delete');
+        // Set attributes and classes for the checkbox, span, and other elements
+        input.type = 'checkbox';
+        input.checked = todo.done;
+        span.classList.add('bubble');
+        content.classList.add('todo-content');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
 
-		// Set the content of the todo content div
-		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+        // Set the content of the todo content div
+        content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
         edit.innerHTML = 'Edit';
-		deleteButton.innerHTML = 'Delete';
-		// Set the labels and append elements to the todo item div
-		label.appendChild(input);
-		label.appendChild(span);
-		actions.appendChild(edit);
-		actions.appendChild(deleteButton);
-		todoItem.appendChild(label);
-		todoItem.appendChild(content);
-		todoItem.appendChild(actions);
+        deleteButton.innerHTML = 'Delete';
 
-		// Append the todo item to the todo list container
-		todoList.appendChild(todoItem);
+        // Set the labels and append elements to the todo item div
+        label.appendChild(input);  // Put the checkbox inside the label
+        label.appendChild(span);
+        content.appendChild(label);  // Move the label inside the todo-content div
+        actions.appendChild(edit);
+        actions.appendChild(deleteButton);
+        todoItem.appendChild(content);
+        todoItem.appendChild(actions);
 
-		// Add 'done' class to the todo item if it is marked as done
-		if (todo.done) {
-			todoItem.classList.add('done');
-		}
+        // Append the todo item to the todo list container
+        todoList.appendChild(todoItem);
 
-		// Event listener for changes in the checkbox
-		input.addEventListener('change', (e) => {
-			// Update the 'done' status of the todo
-			todo.done = e.target.checked;
-			// Store the updated todos array in localStorage
-			localStorage.setItem('todos', JSON.stringify(todos));
+        // Add 'done' class to the todo item if it is marked as done
+        if (todo.done) {
+            todoItem.classList.add('done');
+        }
 
-			// Update the visual representation of the todo item
-			if (todo.done) {
-				todoItem.classList.add('done');
-			} else {
-				todoItem.classList.remove('done');
-			}
+        // Event listener for changes in the checkbox
+        input.addEventListener('change', (e) => {
+            // Update the 'done' status of the todo
+            todo.done = e.target.checked;
+            // Store the updated todos array in localStorage
+            localStorage.setItem('todos', JSON.stringify(todos));
 
-			// Display the updated todos
-			DisplayTodos();
-		})
+            // Update the visual representation of the todo item
+            if (todo.done) {
+                todoItem.classList.add('done');
+            } else {
+                todoItem.classList.remove('done');
+            }
 
-		// Event listener for the 'Edit' button
-		edit.addEventListener('click', (e) => {
-			// Select the input element within the todo content div
-			const input = content.querySelector('input');
-			// Remove the 'readonly' attribute to enable editing
-			input.removeAttribute('readonly');
-			// Set focus on the input element
-			input.focus();
-			// Event listener for when the input loses focus (blurs)
-			input.addEventListener('blur', (e) => {
-				// Set the 'readonly' attribute to disable further editing
-				input.setAttribute('readonly', true);
-				// Update the content of the todo with the edited value
-				todo.content = e.target.value;
-				// Store the updated todos array in localStorage
-				localStorage.setItem('todos', JSON.stringify(todos));
-				// Display the updated todos
-				DisplayTodos();
-			})
-		})
+            // Display the updated todos
+            DisplayTodos();
+        })
 
-		// Event listener for the 'Delete' button
-		deleteButton.addEventListener('click', (e) => {
-			// Remove the todo from the todos array
-			todos = todos.filter(t => t != todo);
-			// Store the updated todos array in localStorage
-			localStorage.setItem('todos', JSON.stringify(todos));
-			// Display the updated todos
-			DisplayTodos();
-		})
-	})
+        // Event listener for the 'Edit' button
+        edit.addEventListener('click', (e) => {
+            // Select the input element within the todo content div
+            const input = content.querySelector('input');
+            // Remove the 'readonly' attribute to enable editing
+            input.removeAttribute('readonly');
+            // Set focus on the input element
+            input.focus();
+            // Event listener for when the input loses focus (blurs)
+            input.addEventListener('blur', (e) => {
+                // Set the 'readonly' attribute to disable further editing
+                input.setAttribute('readonly', true);
+                // Update the content of the todo with the edited value
+                todo.content = e.target.value;
+                // Store the updated todos array in localStorage
+                localStorage.setItem('todos', JSON.stringify(todos));
+                // Display the updated todos
+                DisplayTodos();
+            })
+        })
+
+        // Event listener for the 'Delete' button
+        deleteButton.addEventListener('click', (e) => {
+            // Remove the todo from the todos array
+            todos = todos.filter(t => t != todo);
+            // Store the updated todos array in localStorage
+            localStorage.setItem('todos', JSON.stringify(todos));
+            // Display the updated todos
+            DisplayTodos();
+        })
+    })
 }
